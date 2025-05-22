c0582f53ecb3afe415a37318e025028e517a847d
manjesh453 <rayamajhimanjesh41@gmail.com>
2025-05-22 17:43:01 +0545

    Implement application properties for borrow service

--- book-service/src/main/java/com/bookservice/repo/BookRepo.java
+++ book-service/src/main/java/com/bookservice/repo/BookRepo.java
@@ -3,6 +3,8 @@ package com.bookservice.repo;
 import com.bookservice.entity.Book;
 import com.bookservice.shared.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,6 @@ public interface BookRepo extends MongoRepository<Book, String> {
     List<Book> findByAuthorName(String author);
     List<Book> findByStatus(Status status);
     Integer countByStatus(Status status);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Book> findByTitle(String title);
 }
